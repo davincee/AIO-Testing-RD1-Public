42428ad0097db9ec0cf461997d7c3026a61a55a7
Vince E <VINCE@DELTA.yo.dude.come>
2024-05-14 16:35:12 +0800

    repoCreate.bat

---
+++ AIO-Testing-RD1-Public-repoCreate.bat
@@ -0,0 +1,7 @@
+echo "# AIO-Testing-RD1-Public" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/davincee/AIO-Testing-RD1-Public.git
+git push -u origin main
